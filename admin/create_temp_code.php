3e9d2a041fe903cb3ba2d0303e421926ecd78f4a
George Leigh <mrmichaelkendall@gmail.com>
2020-11-24 19:19:20 +0000

    reformatted password reset copypasta

--- admin/create_temp_code.php
+++ admin/create_temp_code.php
@@ -12,9 +12,14 @@ if (isset($_POST["ChangePW"])) {
     setCode($_POST["txtSteamName"], $_POST["txtNewPass"]);
 ?>
     <h6> Send this message to <?php echo $_POST['discord_name']; ?></h6>
-    <div class="border-p4">
-        Your password to the DTCC Dashboard has been reset. Your new password is: ``` <?php echo $_POST["txtNewPass"] ?>```
-    </div>
+    <p class="border-p4">
+        Your password to the DTCC Dashboard has been reset.<br>
+        All passwords are stored in an encrypted format, and cannot be retrieved by staff.<br>
+        Please use this code next time you login:<br>
+        ``` <?php echo $_POST["txtNewPass"] ?>```
+        Your code will only work once, after which you will be redirected to reset your password.<br>
+        Thanks!
+    </p>
 <?php
     //header("Location: ../index.php");
 }
