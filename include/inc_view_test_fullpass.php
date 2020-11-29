5c1e3f38295f90ad66273ec372690b25395586d3
George Leigh <mrmichaelkendall@gmail.com>
2020-11-29 12:26:40 +0000

    hotfix

--- include/inc_view_test_fullpass.php
+++ include/inc_view_test_fullpass.php
@@ -6,7 +6,7 @@
                 Congratulations <?php echo $rvals['char_name'] ?><br>
                 You are now a member of Downtown Cab Co!<br>
                 Your new callsign is: <?php echo $rvals["callsign"] . " | " . $rvals["char_name"]; ?><br>
-                feel free to join our Offical Discord Server ->https://discord.gg/C79Kgcys<br>
+                feel free to join our Offical Discord Server ->https://discord.gg/C8xjc29WXz<br>
                 Please be patient while we assign you a ***shiny new tag***, you should be able to clock in on the next server restart.<br>
                 We look forward to seeing you on shift!<br>
                 Kind Regards<br>
@@ -16,7 +16,7 @@
         <div class="col-6">
             <h6>Copy/paste for #database-todos:</h6>
             <div class="border text-center p-4 mb-3">
-                Please **remove** DTCC Recruitment tag from @<?php echo $rvals['discord_name']; ?>, and give him the main Downtown Cab Co. tag. Thanks!
+                Please **remove** DTCC Recruitment tag from @<?php echo $rvals['discord_name']; ?>, and give him the Downtown Cab Co. tag. Thanks!
             </div>
             <h6>No Whitelisting access?
                 Post this in #supervisors:</h6>
