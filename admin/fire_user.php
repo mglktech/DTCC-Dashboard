99a776f108bda736a374a5006a48e681d2e2a4a2
George Leigh <mrmichaelkendall@gmail.com>
2020-12-01 16:21:30 +0000

    include firing recruits

--- admin/fire_user.php
+++ admin/fire_user.php
@@ -3,18 +3,19 @@ include "../include/sqlconnection.php";
 
 function CreateReasonPhrase($reason)
 {
-    $str = "";
+
     if ($reason == "inactivity") {
         $str = "You have not turned up for work in a while.";
-    }
-    if ($reason == "too_many_strikes") {
+    } else if ($reason == "too_many_strikes") {
         $str = "You have received too many Strikes.";
-    }
-    if ($reason == "resigned") {
+    } else if ($reason == "resigned") {
         $str = "You have resigned.";
-    }
-    if ($reason == "misconduct") {
+    } else if ($reason == "misconduct") {
         $str = "Gross Misconduct";
+    } else if ($reason == "inactivity_recruit") {
+        $str = "You have not passed your tests in time.";
+    } else if ($reason == "failure_recruit") {
+        $str = "You have failed too many tests.";
     } else {
         $str = $_POST["otherreason"];
     }
@@ -24,6 +25,9 @@ $text = "";
 $BanFire = "Fired";
 $banned = 0;
 if (isset($_POST["FireMe"])) {
+
+
+
     $steam_id = $_POST["SteamID"];
     $signed_by = $_POST["SignedBy"];
     $timestamp = time();
@@ -60,28 +64,36 @@ if (isset($_POST["FireMe"])) {
 
 <div class="container">
     <div class="row">
-        <h4><?php echo $_POST["char_name"] ?> has been <?php echo $BanFire ?> from Downtown Cab Co.</h4>
+        <?php if (isset($_POST["recruit"])) { ?>
+            <h4><?php echo $_POST["char_name"] ?> has been removed from Downtown Cab Co. Recruitment.</h4>
+        <?php } else { ?>
+            <h4><?php echo $_POST["char_name"] ?> has been <?php echo $BanFire ?> from Downtown Cab Co.</h4>
+        <?php } ?>
 
     </div>
     <div class="row">
         <div class="col border">
             <h6>Send this message to <?php echo $_POST['DiscordName'] ?></h6>
-            <p>Hello <?php echo $_POST["char_name"] ?> <br>
+            Hello <?php echo $_POST["char_name"] ?> <br>
+            <?php if (isset($_POST["recruit"])) { ?>
+                We regret to imform you that you have been Removed from Downtown Cab Co. Recruitment.<br>
+            <?php } else { ?>
                 We regret to inform you that you have been <?php echo $BanFire ?> from Downtown Cab Co.<br>
-                The reason given is as follows: <br>
-                <?php echo CreateReasonPhrase($reason); ?><br>
-                <?php if ($banned) { ?>
-                    You are not permitted to reapply, if you do, your application will be rejected automatically.<br>
-                <?php } else { ?>
-                    You are permitted to re-apply at a later date if you wish, our staff are informed of your previous employment with us.<br>
-                <?php } ?>
-                <br>
-                Signed, <?php echo q_fetchPlayerFormatted($signed_by); ?><br>
-                <?php if ($add_info != "") { ?>
-                    Additional Notes: <br>
-                <?php echo $add_info;
-                } ?>
-            </p>
+            <?php } ?>
+            The reason given is as follows: <br>
+            <?php echo CreateReasonPhrase($reason); ?><br>
+            <?php if ($banned) { ?>
+                You are not permitted to reapply, if you do, your application will be rejected automatically.<br>
+            <?php } else { ?>
+                You are permitted to re-apply at a later date if you wish, our staff are informed of your previous employment with us.<br>
+            <?php } ?>
+            <br>
+            Signed, <?php echo q_fetchPlayerFormatted($signed_by); ?><br>
+            <?php if ($add_info != "") { ?>
+                Additional Notes: <br>
+            <?php echo $add_info;
+            } ?>
+
         </div>
         <div class="col border">
             <h6> Send this message to #database-todos</h6>
--- include/inc_sen_super.php
+++ include/inc_sen_super.php
@@ -5,7 +5,12 @@
     <h6>Senior Supervisor Commands:</h6>
 
     <button class="btn bg-danger text-light mb-3" data-toggle="modal" data-target="#ResetCodeModal" <?php if ($rank < 2) echo "disabled" ?>><i class="fas fa-key"></i> Password Reset</button>
-    <button class="btn bg-danger text-light mb-3" data-toggle="modal" data-target="#FireModal" <?php if ($rank >= $_SESSION["rank"] || $rank < 0) echo "disabled" ?>><i class="fas fa-gavel"></i> Fire Employee</button>
+    <?php if ($rank > -1) { ?>
+        <button class="btn bg-danger text-light mb-3" data-toggle="modal" data-target="#FireModal" <?php if ($rank >= $_SESSION["rank"]) echo "disabled" ?>><i class="fas fa-gavel"></i> Fire Employee</button>
+    <?php } else { ?>
+        <button class="btn bg-danger text-light mb-3" data-toggle="modal" data-target="#RemoveRecruitModal" <?php if ($rank >= $_SESSION["rank"]) echo "disabled" ?>><i class="fas fa-gavel"></i> Remove Recruit</button>
+    <?php } ?>
+
 </div>
 
 <!-- RESET CODE MODAL -->
@@ -120,6 +125,75 @@
         </div>
     </form>
 </div>
+
+<div class="modal fade" id="RemoveRecruitModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <form action="admin/fire_user.php" method="post">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">You are about to remove <?php echo $char_name; ?> from Downtown Cab Co. recruitment.</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-6 mt-2 mb-3">
+                                <h5>Reasons for Dismissal</h5>
+
+                                <input type="radio" name="reason" id="inactivity" value="inactivity_recruit">
+                                <label for="inactivity">
+                                    <h6 class="font-weight-normal">Inactivity</h6>
+                                </label><br>
+
+                                <input type="radio" id="failure" name="reason" value="failure_recruit">
+                                <label for="failure">
+                                    <h6 class="font-weight-normal">Failed too many tests</h6>
+                                </label><br>
+
+                                <input type="radio" name="reason" id="otherchk" value="other" data-toggle="collapse" data-target="#othergroup" aria-expanded="false" aria-controls="otherreason">
+                                <label for="otherchk">
+                                    <h6 class="font-weight-normal">Other (Please specify)</h6>
+                                </label><br>
+
+                                <div class="collapse" id="othergroup">
+                                    <div class="input-group my-2 w-75">
+                                        <input type="text" class="form-control" name="otherreason" placeholder="What's your reason?" id="otherreason" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6 mt-2">
+                                <div class="form-group">
+                                    <textarea placeholder="Additional Information" class="form-control" name="additionalInformation" id="additionalInformation" rows="5"></textarea>
+                                </div>
+                                <h6>
+                                    <div class="custom-control custom-switch">
+                                        <input class="custom-control-input" type="checkbox" id="ban" name="ban">
+                                        <label class="custom-control-label" for="ban">
+                                            <h6 class="font-weight-bold text-danger">Ban This Employee</h6>
+                                        </label>
+                                    </div>
+                                </h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <input name="FireMe" value='1' hidden>
+                    <input name="recruit" value='1' hidden>
+                    <input name="DiscordName" value='<?php echo $discord_name ?>' hidden>
+                    <input name="char_name" value=<?php echo $char_name; ?> hidden>
+                    <input name="SteamID" value='<?php echo $steamid; ?>' hidden>
+                    <input name="SignedBy" value='<?php echo $_SESSION["steam_id"]; ?>' hidden>
+                    <button type="submit" class="btn btn-danger">Fire <?php echo explode(" ", $char_name)[0]; ?></button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Go Back</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
 
 <script>
     function generateCode() {
