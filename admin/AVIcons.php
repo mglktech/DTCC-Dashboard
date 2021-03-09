369275a4daba62b062211d8dd06bed4abd3bb6d1
pilotvollmar <mrmichaelkendall@gmail.com>
2021-03-09 22:57:06 +0000

    try again

--- admin/AVIcons.php
+++ admin/AVIcons.php
@@ -1,5 +1,5 @@
 <?php include "../include/components/head.php";
-include "../steam/SteamWebAPI.php";
+
 function sel_all_steamids()
 {
     $sql = Query("SELECT `steam_id` FROM `players` WHERE 1");
@@ -29,5 +29,68 @@ function UpdateAllAVIcons()
         UpdateAVIcons($steamid);
     }
 }
+
+
+function SteamAPIKey()
+{
+    $sql = "SELECT * FROM `secrets` WHERE `secret_name` = 'SteamAPIKey'";
+    return QueryFirst($sql)->secret;
+}
+
+
+
+function GetPlayerSummaries($SteamIDs)
+{
+    $apikey = SteamAPIKey();
+    $base = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=" . $apikey . "&steamids=" . $SteamIDs;
+    return  json_decode(file_get_contents($base), true);
+}
+
+function ResolveVanityUrl($VanityKey)
+{
+    $apikey = SteamAPIKey();
+    $base = "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key=" . $apikey . "&vanityurl=" . $VanityKey;
+    return  json_decode(file_get_contents($base), true);
+}
+
+
+
+function GetSteamNames($steamids) // test this
+{
+    $chunks = array_chunk($steamids, 10);
+    $steam_names = array();
+    foreach ($chunks as $c) {
+        $row = "";
+        foreach ($c as $steamid) {
+            $row += $steamid . ",";
+        }
+        $summaries = GetPlayerSummaries($row)["response"]["players"];
+        foreach ($summaries as $summary) {
+            $steam_names[] = $summary["personaname"];
+        }
+    }
+    return $steam_names;
+}
+
+function GetSteamDetails($SteamID)
+{
+    // need to collect personaname, avatar, avatarmedium, avatarfull and pair with steamid
+    $player = new stdClass();
+    $player->steam_name = null;
+    $player->steam_id = null;
+    $player->av_icon = null;
+    $player->av_medium = null;
+    $player->av_full = null;
+    if ($SteamID) {
+        $summaries = getPlayerSummaries($SteamID);
+        $player->steam_name = $summaries["response"]["players"][0]["personaname"];
+        $player->steam_id = $summaries["response"]["players"][0]["steamid"];
+        $player->av_icon = $summaries["response"]["players"][0]["avatar"];
+        $player->av_medium = $summaries["response"]["players"][0]["avatarmedium"];
+        $player->av_full = $summaries["response"]["players"][0]["avatarfull"];
+    }
+    return $player;
+}
+
 UpdateAllAVIcons();
 include "../include/components/foot.php";
