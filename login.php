4156c5629e57d9f5b1661c542b78eb42e6039867
pilotvollmar <mrmichaelkendall@gmail.com>
2021-03-09 23:56:32 +0000

    removing old code

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
 <?php
 include "include/components/head.php";
-include "steam/SteamWebAPI_Simple.php";
 
 isset($_SESSION["error"]) ? $error = $_SESSION["error"] : $error = "";
 function chkOnline()
@@ -16,7 +15,6 @@ function chkOnline()
 
 function response($r = null)
 {
-
 }
 
 if (isset($_POST["Login"])) {
@@ -27,8 +25,7 @@ if (isset($_POST["Login"])) {
     //BeginSession(q_fetchPlayer(getSteamID($user)), false);
     if ($response) {
         BeginSession(q_fetchPlayer(getSteamID($user)), true);
-    }
-    else {
+    } else {
         $response2 = check_password($user, $pass);
         if ($response2) {
             BeginSession(q_fetchPlayer(getSteamID($user)), false);
@@ -44,20 +41,20 @@ if (isset($_POST["Login"])) {
 
 <div class="container-fluid d-flex justify-content-center justify-content-sm-center align-items-sm-center justify-content-lg-center" style="min-height: 39vh;">
     <form method="post" style="text-align: center;padding: 57px;max-width: 392px;height: 278px;"><label style="color: rgb(255,255,255);">Please Login Below.</label>
-    <label style="color: rgb(255,255,255);"></label>
+        <label style="color: rgb(255,255,255);"></label>
         <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
         <div class="form-group"><input class="form-control" type="text" name="steam_name" placeholder="Steam Name"></div>
         <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
         <div class="form-group"><button class="btn btn-primary btn-block " name="Login" type="submit" style="border-style: none;background: rgb(34,77,124);">Log In</button></div>
     </form>
-    
+
 </div>
 <div class="container">
-<label>MySQL is: <?php if (chkOnline()) {
-                        echo "online";
-                    } else {
-                        echo "offline";
-                    } ?></label>
+    <label>MySQL is: <?php if (chkOnline()) {
+                            echo "online";
+                        } else {
+                            echo "offline";
+                        } ?></label>
 </div>
 
 
