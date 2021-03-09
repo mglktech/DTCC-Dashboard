9d51c023d7e2974b99e7189ff3c5e4bfe8c6d8e2
pilotvollmar <mrmichaelkendall@gmail.com>
2021-03-09 22:35:28 +0000

    why does linux have to be funny with caps

--- admin/AVIcons.php
+++ admin/AVIcons.php
@@ -1,5 +1,5 @@
 <?php include "../include/components/head.php";
-include "../steam/SteamWebApi_Simple.php";
+include "../steam/steamwebapi_simple.php";
 function sel_all_steamids()
 {
     $sql = Query("SELECT `steam_id` FROM `players` WHERE 1");
