1540f38f58aedcf8563b4a4cfe3e5a8d4dad3aa1
pilotvollmar <mrmichaelkendall@gmail.com>
2021-03-11 11:13:34 +0000

    linux caps sensitivity is cap

--- admin/upd_player_details.php
+++ admin/upd_player_details.php
@@ -1,5 +1,5 @@
 <?php include "../include/components/head.php";
-include "../steam/SteamWebAPI.php";
+include "../steam/SteamWebApi.php";
 include "../include/elements.php";
 
 isset($_GET["steamid"]) ? $steamid = quotefix($_GET["steamid"]) : $steamid = "76561197995263974";
