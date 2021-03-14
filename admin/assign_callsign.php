cb13e12e6efe09f7752912273d1449793828e63d
pilotvollmar <mrmichaelkendall@gmail.com>
2021-03-14 16:44:56 +0000

    fix: callsign assignment

--- admin/assign_callsign.php
+++ admin/assign_callsign.php
@@ -2,30 +2,27 @@
 include "../include/components/head.php";
 isset($_GET["id"]) ? $id = $_GET["id"] : $id = null;
 
-if(isset($_POST["callsign"]))
-{
-    AssignCallsign($id,$_POST["callsign"]);
+if (isset($_POST["callsign"])) {
+    AssignCallsign($id, $_POST["callsign"]);
 }
 
-function AssignCallsign($id,$label)
+function AssignCallsign($id, $label)
 {
     Query("UPDATE `callsigns` SET `assigned_steam_id`='$id' where `label` = '$label'");
 }
 
 function IdHasCallsign($id)
 {
-    if($id)
-    {
+    if ($id) {
         return QueryFirst("SELECT * FROM `callsigns` WHERE `assigned_steam_id` = '$id'");
-    }
-    else {
+    } else {
         return null;
     }
 }
 
 function CollectCallsigns($rank, $region)
 {
-    $sql = "SELECT * FROM `callsigns` WHERE `min_rank` = $rank AND region = '$region' AND assigned_steam_id is null limit 10";
+    $sql = "SELECT * FROM `callsigns` WHERE `min_rank` = '$rank' AND `region` = '$region' AND assigned_steam_id is null limit 10";
 
     return Query($sql);
 }
@@ -33,33 +30,31 @@ function CollectCallsigns($rank, $region)
 
 
 $player = QueryFirst("SELECT * FROM `public_players` WHERE `steam_id` = '$id'");
-$available_callsigns = CollectCallsigns($player->rank,$player->timezone);
+$available_callsigns = CollectCallsigns($player->rank, $player->timezone);
 $existing_callsign = IdHasCallsign($id);
 $latest_test = QueryFirst("SELECT * FROM `tests` WHERE `steam_id` = '$id' ORDER BY `id` DESC");
 ?>
 <div class="container">
-        <div class="row">
-            <div class="col d-flex flex-column justify-content-center align-items-center">
-                <h1>Assign Callsign</h1>
-            </div>
+    <div class="row">
+        <div class="col d-flex flex-column justify-content-center align-items-center">
+            <h1>Assign Callsign</h1>
         </div>
-        <div class="row">
-            <div class="col d-flex flex-column align-items-end">
-                <h5>You are assigning a callsign for:</h5>
-            </div>
-            <div class="col d-flex flex-column justify-content-center align-items-start align-content-center">
-                <h5><?=$player->char_name?></h5>
-            </div>
-        </div>
-        <?php if($existing_callsign) {
-            include "elems/callsign_already_assigned.php";
-        }
-        else {
-            include "elems/callsign_part.php";
-        }?>
-        
     </div>
+    <div class="row">
+        <div class="col d-flex flex-column align-items-end">
+            <h5>You are assigning a callsign for:</h5>
+        </div>
+        <div class="col d-flex flex-column justify-content-center align-items-start align-content-center">
+            <h5><?= $player->char_name ?></h5>
+        </div>
+    </div>
+    <?php if ($existing_callsign) {
+        include "elems/callsign_already_assigned.php";
+    } else {
+        include "elems/callsign_part.php";
+    } ?>
+
+</div>
 
 
 <?php include "../include/components/foot.php";
-
